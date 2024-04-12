9149d3342f56daf7218530c7f21460c37e279f13
Daniel Fu <daniel.fu123@hotmail.com>
2024-04-12 14:06:23 -0400

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,13 @@
+import { Container } from 'react-bootstrap';
+import MainNav from './MainNav';
+
+const Layout = (props) => (
+  <>
+    <MainNav />
+    <br />
+    <Container>{props.children}</Container>
+    <br />
+  </>
+);
+
+export default Layout;
